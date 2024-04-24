2cc16690e343cc60dc985a3e8297cda7d016fa7f
Bustah <psnatb2lme@gmail.com>
2024-04-24 22:50:11 +0800

    Added Music

--- character-selection/src/MainClass.java
+++ character-selection/src/MainClass.java
@@ -1,12 +1,22 @@
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 //damnn eto packages, para magamit yung character-selection-package
 import characterselection.*;
 import characterselection.SplashScreen;
 
 public class MainClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+        // these four lines setup for the music but don't play it, music play command is in line 47
+        File file = new File("character-selection/src/res/Music/BGM.wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+
         // 1st - this show the splash screen muna
         SplashScreen.show();
 
@@ -34,6 +44,7 @@ public class MainClass {
         // 2nd - then after splashscreen papakita nmn main menu
         MainMenu.show(mainFrame, "character-selection/src/res/game-background/background-2.gif");
 
-        
+        // Hi, I'm music play command
+        clip.start();
     }
 }
