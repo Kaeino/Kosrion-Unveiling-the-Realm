5ee4361d89cd0b7114bef85901efd6f8bd39dda3
Bustah <psnatb2lme@gmail.com>
2024-04-25 08:51:03 +0800

    DAMNNNN BUTTONSSS

--- character-selection/src/characterselection/MainMenu.java
+++ character-selection/src/characterselection/MainMenu.java
@@ -2,7 +2,7 @@ package characterselection;
 
 import javax.swing.*;
 
-import mainselection.MainSelection;
+import mainselection.*;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -13,8 +13,20 @@ public class MainMenu {
     // Damnn eto yung method para mapakita yung main menu namay background image, which is yung lone hero, maintframe + imagepath = main menu with image damnn
     public static void show(JFrame mainFrame, String backgroundImagePath) {
 
+        // Damnn, eto nmn hinohold nya lang main menu BG, parang object shit
+        JPanel mainMenuBg = new JPanel(new BorderLayout());
+
         // Damnn, eto nmn hinohold nya lang main menu components, parang object shit
         JPanel mainMenuPanel = new JPanel(new BorderLayout());
+
+        // Damnn, eto is para transparent ung mainMenuPanel
+        mainMenuPanel.setBackground(new Color(0,0,0,0));
+
+        // Damnn, eto is para macover nung mainMenuPanel ung buong screen
+        mainMenuPanel.setBounds(0,0,950,500);
+
+        // damnn adding it sa taas ng mainmenubg
+        mainMenuBg.add(mainMenuPanel);
 
         // Damn eto nmn label pang display ng Background Image
         JLabel backgroundLabel = new JLabel();
@@ -27,10 +39,10 @@ public class MainMenu {
         backgroundLabel.setIcon(backgroundImage);
 
         // After nmn background label aadd nya nmn sa center ng mainmenu panel yung bg label, para gumitna lang
-        mainMenuPanel.add(backgroundLabel, BorderLayout.CENTER);
+        mainMenuBg.add(backgroundLabel, BorderLayout.CENTER);
 
         // Create a panel for buttons
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 50, 10));
 
         // code for create a new character button lang
         // code for create a new character button lang
@@ -43,19 +55,20 @@ public class MainMenu {
         createCharacterButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            // button is clicked? then will go open the MainSelection.java file
-             MainSelection.show(mainFrame);
+            // button is clicked? then will go open the MainSelection.java file (IMMA CONTINUE THIS NYAN) -P
+          //   MainSelection.show(mainFrame);
             }
         });
 
 
         // Add the create character button to the button panel
         buttonPanel.add(createCharacterButton);
+        
 
         // whole code for exit button lang
         ImageIcon quitIcon = new ImageIcon("character-selection/src/res/game-background/quit-button_dummy.png");
         JButton quitButton = new JButton(quitIcon);
-        quitButton.setPreferredSize(new Dimension(140, 50));
+        quitButton.setPreferredSize(new Dimension(150, 50));
         quitButton.setContentAreaFilled(false); // 
         quitButton.setBorderPainted(false); // just removing the border, useless
         quitButton.setOpaque(false); // Set button background to be transparent daw tae
@@ -71,7 +84,10 @@ public class MainMenu {
         // Add the quit button to the button panel
         buttonPanel.add(quitButton);
 
+        buttonPanel.setBackground(new Color(0,0,0,0));
+
         // Add the button panel to the main menu panel
+
         mainMenuPanel.add(buttonPanel, BorderLayout.SOUTH);
 
         // Eto mini shits
@@ -80,7 +96,7 @@ public class MainMenu {
         mainFrame.getContentPane().removeAll();
 
         // Inaadd nmn nito yung main menu panel sa main frame
-        mainFrame.getContentPane().add(mainMenuPanel);
+        mainFrame.getContentPane().add(mainMenuBg);
 
         // Damn revalidate means inaupdate nya lang yung layout ng frame
         mainFrame.revalidate();
